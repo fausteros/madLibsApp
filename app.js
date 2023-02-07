c47741bc49959ded0c9327f8568c83e4d4a17cf3
thxeris <bioeris@gmail.com>
2023-02-07 11:04:33 -0500

    Next commit, added first variables and values

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const qstnTtl = 20;
+
+let qstnCnt = 0;
+
+let prmpt = `Please, enter a`
+let wordsArray = [
+
+    
+]
+
+
+let originalStory = 0;
+let distortedStory = 0;
+
+
+
+
+
+
+
+
+
---
+++ app2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let userInputs = 0;
+let adjectives = [
+    "funny",
+    "happy",
+    "silly",
+    "excited",
+    "sloppy",
+    "generic",
+    "cheerful",
+    "charming",
+    "clever",
+    "cloudy",
+    "clumsy",
+    "clean",
+    "crazy",
+    "cute",
+]
+
+let originalStory = 0;
+
+
+// let originalStory = 0;
+
+// Don't forget to use prompt and alert fucntions
+// To ask/display information
