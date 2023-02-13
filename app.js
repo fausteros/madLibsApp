8652ee84c47b1555b01a4f8adb8b1c12521fd59b
zaeris2 <45835580+zaeris2@users.noreply.github.com>
2023-02-13 12:10:37 -0500

    4th Added basic syntax and placeholders

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 "use strict";
 
-const qstnTtl = 20;
+let questionsTotal = 15;
 
-let qstnCnt = 0;
+let userInputs = [];
 
-let prmpt = `Please, enter a`
-let wordsArray = [
+let questionCount = 0;
+
+let prompt = `Please, enter a`
+let wordsArray = [ 
 
     
 ]
 
 
-let originalStory = 0;
 let distortedStory = 0;
 
 
 
 
 
+let originalStory1 = `Once upon a time, there was a ${userInputs[0]} prince who lived in a ${userInputs[1]} castle. One day, while he was out hunting, he came across a ${userInputs[2]} ${userInputs[3]} who granted him three wishes.
+The prince wished for a (adjective) horse, a (adjective) sword, and the hand of the (adjective) princess from the neighboring kingdom.
+With his new gifts, the prince rode off to the princess's palace. When he arrived, he found that she had been taken by a(n) (adjective) dragon! The prince bravely fought the dragon and saved the princess, who was so grateful that she agreed to marry him.
+And so, the prince and princess lived happily ever after in their (adjective) kingdom, with their (adjective) horse, sword, and each other by their side.`;
 
 
 
 
+
+// let originalStory = `Once upon a time, there was a (adjective) prince who lived in a (adjective) castle. One day, while he was out hunting, he came across a (adjective) (noun) who granted him three wishes.
+// The prince wished for a (adjective) horse, a (adjective) sword, and the hand of the (adjective) princess from the neighboring kingdom.
+// With his new gifts, the prince rode off to the princess's palace. When he arrived, he found that she had been taken by a(n) (adjective) dragon! The prince bravely fought the dragon and saved the princess, who was so grateful that she agreed to marry him.
+// And so, the prince and princess lived happily ever after in their (adjective) kingdom, with their (adjective) horse, sword, and each other by their side.`;
